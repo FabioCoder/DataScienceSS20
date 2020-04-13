9dbe040f844658ca3215782805a0956e9363e6b8
Fabio Kraemer <fkraemer@stud.hs-offenburg.de>
2020-04-13 12:08:50 +0200

    Add ListkeeperModule

--- Exercises/ListkeeperModule.py
+++ Exercises/ListkeeperModule.py
@@ -1,1 +1,26 @@
 
+class Listkeeper:
+    def __init__():
+        """initializes the dictionary with a list named example"""
+        self.dict = {"example": [1,2,3,4,5]}
+
+    def show():
+        """returns all list names"""
+        for x in dict:
+            print(x)
+
+    def add(self,name, list):
+        """adds a new list"""
+        dict[name]: list
+
+    def delete(self,name):
+        """deletes list"""
+        dict.pop(name)
+
+    def sort (self, name):
+        """returns the sorted list *name*"""
+        return dict[name].sort()
+
+    def append (self, name, list):
+        """ appends *list* to *name* """
+        dict[name].extend(list)
